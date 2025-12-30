0ec4fa887cc1faa61a91a9895c9b0fb8db0f5f4e
Armando Romero <JArmandoRomero@icloud.com>
2025-12-30 15:26:08 -0600

    fix logout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
+import { UserProvider } from "./context/UserContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 import Sidebar from "./components/Sidebar";
@@ -13,34 +14,38 @@ import Register from "./pages/Register";
 
 export default function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+    <BrowserRouter>
+      <AuthProvider>
+        <UserProvider>
+          <Routes>
+            {/* Public routes */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
 
-          <Route
-            path="/*"
-            element={
-              <ProtectedRoute>
-                <div className="flex min-h-screen">
-                  <Sidebar />
-                  <div className="flex-1">
-                    <Navbar />
-                    <main className="p-6">
-                      <Routes>
-                        <Route path="/" element={<Dashboard />} />
-                        <Route path="/users" element={<Users />} />
-                        <Route path="/settings" element={<Settings />} />
-                      </Routes>
-                    </main>
+            {/* Protected layout */}
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <div className="flex min-h-screen">
+                    <Sidebar />
+                    <div className="flex-1">
+                      <Navbar />
+                      <main className="p-6">
+                        <Routes>
+                          <Route index element={<Dashboard />} />
+                          <Route path="users" element={<Users />} />
+                          <Route path="settings" element={<Settings />} />
+                        </Routes>
+                      </main>
+                    </div>
                   </div>
-                </div>
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </UserProvider>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
