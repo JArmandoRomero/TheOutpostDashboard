08e71ecc9c9d36ca1735c67dad9db3324f53e9df
Armando Romero <JArmandoRomero@icloud.com>
2025-12-30 15:36:41 -0600

    fix logout

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import { AuthProvider } from "./context/AuthContext";
 import { UserProvider } from "./context/UserContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 
-import Sidebar from "./components/Sidebar";
-import Navbar from "./components/Navbar";
+import AdminLayout from "./components/AdminLayout";
 
 import Dashboard from "./pages/Dashboard";
 import Users from "./pages/Users";
@@ -14,35 +13,26 @@ import Register from "./pages/Register";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/admin">
       <AuthProvider>
         <UserProvider>
           <Routes>
-            {/* Public routes */}
+            {/* Public */}
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
 
-            {/* Protected layout */}
+            {/* Protected Admin */}
             <Route
-              path="/"
               element={
                 <ProtectedRoute>
-                  <div className="flex min-h-screen">
-                    <Sidebar />
-                    <div className="flex-1">
-                      <Navbar />
-                      <main className="p-6">
-                        <Routes>
-                          <Route index element={<Dashboard />} />
-                          <Route path="users" element={<Users />} />
-                          <Route path="settings" element={<Settings />} />
-                        </Routes>
-                      </main>
-                    </div>
-                  </div>
+                  <AdminLayout />
                 </ProtectedRoute>
               }
-            />
+            >
+              <Route index element={<Dashboard />} />
+              <Route path="users" element={<Users />} />
+              <Route path="settings" element={<Settings />} />
+            </Route>
           </Routes>
         </UserProvider>
       </AuthProvider>
---
+++ src/components/AdminLayout.jsx
@@ -0,0 +1,17 @@
+import Sidebar from "./Sidebar";
+import Navbar from "./Navbar";
+import { Outlet } from "react-router-dom";
+
+export default function AdminLayout() {
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <div className="flex-1">
+        <Navbar />
+        <main className="p-6">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+}
